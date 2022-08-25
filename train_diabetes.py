afd4dfb53a4be64e9d1a677f8203b108447aa800
Bannasorn Paspanthong <bannasornpaspanthong2023@u.northwestern.edu>
2022-08-25 13:05:31 +0700

    Update train_diabetes.py
    Fix code to match MLflow demo on its website.

--- train_diabetes.py
+++ train_diabetes.py
@@ -70,53 +70,36 @@ if __name__ == "__main__":
 
     alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.05
     l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.05
+    
+    with mlflow.start_run():
+        # Run ElasticNet
+        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+        lr.fit(train_x, train_y)
+        predicted_qualities = lr.predict(test_x)
+        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
 
-    # Run ElasticNet
-    lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
-    lr.fit(train_x, train_y)
-    predicted_qualities = lr.predict(test_x)
-    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+        # Print out ElasticNet model metrics
+        print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+        print("  RMSE: %s" % rmse)
+        print("  MAE: %s" % mae)
+        print("  R2: %s" % r2)
 
-    # Print out ElasticNet model metrics
-    print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
-    print("  RMSE: %s" % rmse)
-    print("  MAE: %s" % mae)
-    print("  R2: %s" % r2)
+        # Log mlflow attributes for mlflow UI
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("l1_ratio", l1_ratio)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("r2", r2)
+        mlflow.log_metric("mae", mae)
 
-    # Log mlflow attributes for mlflow UI
-    mlflow.log_param("alpha", alpha)
-    mlflow.log_param("l1_ratio", l1_ratio)
-    mlflow.log_metric("rmse", rmse)
-    mlflow.log_metric("r2", r2)
-    mlflow.log_metric("mae", mae)
-    mlflow.sklearn.log_model(lr, "model")
+        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
-    # Compute paths
-    eps = 5e-3  # the smaller it is the longer is the path
+        # Model registry does not work with file store
+        if tracking_url_type_store != "file":
 
-    print("Computing regularization path using the elastic net.")
-    alphas_enet, coefs_enet, _ = enet_path(X, y, eps=eps, l1_ratio=l1_ratio)
-
-    # Display results
-    fig = plt.figure(1)
-    ax = plt.gca()
-
-    colors = cycle(["b", "r", "g", "c", "k"])
-    neg_log_alphas_enet = -np.log10(alphas_enet)
-    for coef_e, c in zip(coefs_enet, colors):
-        l2 = plt.plot(neg_log_alphas_enet, coef_e, linestyle="--", c=c)
-
-    plt.xlabel("-Log(alpha)")
-    plt.ylabel("coefficients")
-    title = "ElasticNet Path by alpha for l1_ratio = " + str(l1_ratio)
-    plt.title(title)
-    plt.axis("tight")
-
-    # Save figures
-    fig.savefig("ElasticNet-paths.png")
-
-    # Close plot
-    plt.close(fig)
-
-    # Log artifacts (output files)
-    mlflow.log_artifact("ElasticNet-paths.png")
+            # Register the model
+            # There are other ways to use the Model Registry, which depends on the use case,
+            # please refer to the doc for more information:
+            # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+            mlflow.sklearn.log_model(lr, "model", registered_model_name="ElasticnetWineModel")
+        else:
+            mlflow.sklearn.log_model(lr, "model")
